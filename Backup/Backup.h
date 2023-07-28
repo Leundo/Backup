3dd51f3c1b0e726157800fd13359b762462f3a49
Leundo <daysundoing@outlook.com>
2023-07-28 19:43:34 +0800

    Initial Commit

---
+++ Backup/Backup.h
@@ -0,0 +1,18 @@
+//
+//  Backup.h
+//  Backup
+//
+//  Created by Undo Hatsune on 2023/07/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Backup.
+FOUNDATION_EXPORT double BackupVersionNumber;
+
+//! Project version string for Backup.
+FOUNDATION_EXPORT const unsigned char BackupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Backup/PublicHeader.h>
+
+
